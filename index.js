ffb5243ed4360a17ea54f5b2e33b1a5e1ad4eaa0
Underbase <122473829+khangha49st@users.noreply.github.com>
2024-05-18 18:52:28 +0000

    perf: added path package

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
